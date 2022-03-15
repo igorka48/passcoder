81817c3dd4d4067b6ed57a88d156fa529a0a038f
Ihor Mishchenko <owlsdevelopers@gmail.com>
2022-03-16 00:01:56 +0200

    cleanup

--- app/src/main/java/owlsdevelopers/org/passcoder/data/repository/FirebaseSessionRepository.kt
+++ app/src/main/java/owlsdevelopers/org/passcoder/data/repository/FirebaseSessionRepository.kt
@@ -9,45 +9,27 @@ import owlsdevelopers.org.passcoder.domain.models.Credential
 import owlsdevelopers.org.passcoder.domain.models.User
 import owlsdevelopers.org.passcoder.domain.repository.SessionRepository
 
-class FirebaseSessionRepository(val context: Context,
-                                private val googleSignInRepository: GoogleSignInRepository,
-                                private val firebaseAuth: FirebaseAuth,
-                                private val androidFirebaseAuthenticator: AndroidFirebaseAuthenticator) : SessionRepository {
+class FirebaseSessionRepository(
+    val context: Context, private val firebaseAuth: FirebaseAuth,
+    private val androidFirebaseAuthenticator: AndroidFirebaseAuthenticator
+) : SessionRepository {
 
     override suspend fun createSession(credential: Credential): User {
         val fbUser = androidFirebaseAuthenticator.signIn()
         fbUser?.let {
-          return  User.fromFirebase(it)
+            return User.fromFirebase(it)
         } ?: throw UserNotFoundError()
-
-//        return suspendCoroutine { continuation ->
-//            val googleCredential = GoogleAuthProvider.getCredential(credential.token, null)
-//            firebaseAuth.signInWithCredential(googleCredential)
-//                    .addOnSuccessListener {
-//                        // Sign in success, update UI with the signed-in user's information
-//                        Log.d(MainActivity.TAG, "signInWithCredential:success")
-//                        firebaseAuth.currentUser?.let {
-//                            continuation.resume( User.fromFirebase(it))
-//                        }
-//                    }
-//                    .addOnFailureListener {
-//                        // If sign in fails, display a message to the user.
-//                        Log.w(MainActivity.TAG, "signInWithCredential:failure", it)
-//                        continuation.resumeWithException(it)
-//                    }
-//        }
     }
 
     override suspend fun getSession(): User? {
         val fbUser = firebaseAuth.currentUser
         return if (fbUser != null) {
-            User(fbUser.uid, fbUser.displayName ?: "", fbUser.photoUrl.toString())
+            User(fbUser.uid, fbUser.displayName ?: "", fbUser.photoUrl?.toString() ?: "")
         } else null
     }
 
     override suspend fun deleteSession(): Boolean {
         FirebaseAuth.getInstance().signOut()
-        //return googleSignInRepository.signOut()
         return true
     }
 }
--- app/src/main/java/owlsdevelopers/org/passcoder/presentation/di/DModule.kt
+++ app/src/main/java/owlsdevelopers/org/passcoder/presentation/di/DModule.kt
@@ -6,7 +6,6 @@ import owlsdevelopers.org.passcoder.data.android.util.AndroidFirebaseAuthenticat
 import owlsdevelopers.org.passcoder.data.repository.AndroidClipboardRepository
 import owlsdevelopers.org.passcoder.data.repository.FirebasePasscodeRepository
 import owlsdevelopers.org.passcoder.data.repository.FirebaseSessionRepository
-import owlsdevelopers.org.passcoder.data.repository.GoogleSignInRepository
 import owlsdevelopers.org.passcoder.domain.repository.ClipboardRepository
 import owlsdevelopers.org.passcoder.domain.repository.PasscodeRepository
 import owlsdevelopers.org.passcoder.domain.repository.SessionRepository
@@ -17,7 +16,6 @@ val dataModule = module {
             factory { FirebaseAuth.getInstance() }
             single<PasscodeRepository> { FirebasePasscodeRepository(get()) }
             single<ClipboardRepository> { AndroidClipboardRepository(get()) }
-            single<SessionRepository> { FirebaseSessionRepository(get(), get(), get(), get()) }
-            single{ GoogleSignInRepository(get()) }
+            single<SessionRepository> { FirebaseSessionRepository(get(), get(), get()) }
             single { AndroidFirebaseAuthenticator() }
         }
